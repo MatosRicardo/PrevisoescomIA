b830cd49879623b8b177ead9c7283d8c775e4432
MatosRicardo <ricardoribeirodematos@gmail.com>
2025-01-16 14:23:12 -0300

    Etapa 3 : Treiando a IA

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Passo a passo
 # Passo 0: Entender a empresa e o desafio dela
+#pandas e scikit-learn é para usar a IA
+
 # Passo 1: Importar a base de dados 
 import pandas as pd
 
@@ -20,17 +22,34 @@ from sklearn.preprocessing import LabelEncoder
 # dentista - 4
 # artista - 5
 
+codificador_profissao = LabelEncoder()
+tabela["profissao"] = codificador_profissao.fit_transform(tabela["profissao"])
 
-# engenheiro
-# dentista
-# artista 
+#mix_credito
+codificador_credito = LabelEncoder()
+tabela["mix_credito"] = codificador_credito.fit_transform(tabela["mix_credito"])
 
+#comportamento_pagamento
+codificador_pagamento = LabelEncoder()
+tabela["comportamento_pagamento"] = codificador_pagamento.fit_transform(tabela["comportamento_pagamento"])
+
+# y -> É a coluna da base de dados que eu quero prever
+y = tabela["score_credito"]
+
+# x -> As colunas da base de dados que eu vou usar para fazer previsão
+x = tabela.drop(columns="score_credito")
+
+# Separar em dados de treino e dados de teste
+from sklearn.model_selection import train_test_split
+
+x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.3)
 
 # Passo 3: Treinar a IA -> Criar o modelo: Nota de crédito: Boa, Ok, Ruim
+
+
 # Passo 4: Escolher qual o melhor modelo
 # Passo 5: Usar a IA para fazer novas previsões
 
-#pandas e scikit-learn é para usar a IA
 
 #Score de crédito = Nota de crédito
 
