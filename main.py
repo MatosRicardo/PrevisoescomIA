6316c7d10844313ce11a3e478b130ac42ed9c10a
MatosRicardo <ricardoribeirodematos@gmail.com>
2025-01-16 15:24:15 -0300

    Projeto Finalizado

--- main.py
+++ main.py
@@ -33,13 +33,13 @@ tabela["mix_credito"] = codificador_credito.fit_transform(tabela["mix_credito"])
 codificador_pagamento = LabelEncoder()
 tabela["comportamento_pagamento"] = codificador_pagamento.fit_transform(tabela["comportamento_pagamento"])
 
-# y -> É a coluna da base de dados que eu quero prever
+# y -> é a coluna da base de dados que eu quero prever
 y = tabela["score_credito"]
 
-# x -> As colunas da base de dados que eu vou usar para fazer previsão
-x = tabela.drop(columns="score_credito")
+# x -> as colunas da base de dados que eu vou usar pra fazer a previsão
+x = tabela.drop(columns=["score_credito", "id_cliente"])
 
-# Separar em dados de treino e dados de teste
+# separar em dados de treino e dados de teste
 from sklearn.model_selection import train_test_split
 
 x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.3)
@@ -61,8 +61,29 @@ modelo_knn.fit(x_treino, y_treino)
 
 
 # Passo 4: Escolher qual o melhor modelo
+# Testar a IA
+
+previsao_arvoredecisao = modelo_arvoredecisao.predict(x_teste)
+previsao_knn = modelo_knn.predict(x_teste)
+
+# acurácia
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_teste, previsao_arvoredecisao))
+print(accuracy_score(y_teste, previsao_knn))
+
 # Passo 5: Usar a IA para fazer novas previsões
+tabela_novos_clientes = pd.read_csv("novos_clientes.csv")
 
+# Profissão
+tabela_novos_clientes["profissao"] = codificador_profissao.transform(tabela_novos_clientes["profissao"])
+# Mix_credito
+tabela_novos_clientes["mix_credito"] = codificador_credito.transform(tabela_novos_clientes["mix_credito"])
+# Comportamento_pagamento
+tabela_novos_clientes["comportamento_pagamento"] = codificador_pagamento.transform(tabela_novos_clientes["comportamento_pagamento"])
 
-#Score de crédito = Nota de crédito
+print(tabela_novos_clientes)
+
+nova_previsao = modelo_arvoredecisao.predict(tabela_novos_clientes) 
+print(nova_previsao)
+
 
